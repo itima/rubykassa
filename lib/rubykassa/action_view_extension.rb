607da603bca0c342ae5cc1b43ccd67de5b781d2c
Alexey Vorobiev <vorobiev.alexey@gmail.com>
2014-06-26 23:57:48 +0400

    failed type in fix

--- lib/rubykassa/action_view_extension.rb
+++ lib/rubykassa/action_view_extension.rb
@@ -5,7 +5,7 @@ module Rubykassa
       total, invoice_id  = total.to_s, invoice_id.to_s
       extra_params  = options.except([:custom, :html])
       custom_params = options[:custom] ||= {}
-      html_prams = options[:html] ||= {}
+      html_params = options[:html] ||= {}
       link_to phrase, Rubykassa.pay_url(invoice_id, total, custom_params, extra_params), html_params
     end
   end
